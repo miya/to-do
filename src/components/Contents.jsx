127ccef2fceb14b1a33ada604d25fdf616868bfd
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-01 21:39:12 +0900

    wip

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,11 +1,19 @@
-// import React from 'react';
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import AddText from './AddText'
 
 const Contents = () => {
   return (
     <>
-      <p>Hello To-Do!</p>
+      <Card className="mx-3 mt-3">
+        <Card.Body>
+
+        <AddText />
+          
+        </Card.Body>
+      </Card>
     </>
   );
 };
 
-export default Contents
+export default Contents;
