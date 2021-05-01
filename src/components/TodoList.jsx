427bfcdcea316c5cf6e765232df474a367661e7e
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-01 23:10:24 +0900

    create TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TodoList = (props) => {
+  const { todoList } = props;
+
+  return (
+    <>
+      
+    </>
+  )
+};
+
+TodoList.propTypes = {
+  todoList: PropTypes.array,
+}
+
+export default TodoList;
+
