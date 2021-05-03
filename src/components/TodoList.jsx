199fd1f231afa41c73fbc5da84b28f2af54282a1
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-03 21:12:52 +0900

    fix-task-style

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,22 +8,24 @@ const TodoList = (props) => {
 
   return (
     <>
-      <Card>
-        <ListGroup>
-          {todoList.map((todo, index) => {
-            return (
-              <TodoItem
-                // eslint-disable-next-line react/no-array-index-key
-                key={index}
-                todo={todo}
-                index={index}
-                deleteTodoListHandler={deleteTodoListHandler}
-                changeTodoListHandler={changeTodoListHandler}
-              />
-            );
-          })}
-        </ListGroup>
-      </Card>
+      {todoList.length !== 0 && (
+        <Card className="mt-3">
+          <ListGroup>
+            {todoList.map((todo, index) => {
+              return (
+                <TodoItem
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={index}
+                  todo={todo}
+                  index={index}
+                  deleteTodoListHandler={deleteTodoListHandler}
+                  changeTodoListHandler={changeTodoListHandler}
+                />
+              );
+            })}
+          </ListGroup>
+        </Card>
+      )}
     </>
   );
 };
