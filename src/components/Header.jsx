95f65ee8f2951692474e845681405f5388051f30
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-03 15:34:37 +0900

    fix lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
     <>
       <Navbar expend="md" bg="dark" variant="dark">
         <Navbar.Brand>
-          <img src={`${process.env.PUBLIC_URL}/Logo.png`} width="20" height="20" alt="logo"/>{' '}
+          <img src={`${process.env.PUBLIC_URL}/Logo.png`} width="20" height="20" alt="logo" />{' '}
           To-Do
         </Navbar.Brand>
       </Navbar>
     </>
-  )
-}
+  );
+};
 
 export default Header;
--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
-import TodoForm from './TodoForm'
-import TodoList from './TodoList'
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 const TodoBox = () => {
   const [todo, setTodo] = useState('');
@@ -10,17 +10,22 @@ const TodoBox = () => {
 
   const initTodoList = () => {
     const defaultTodoList = [
-      {text: "task-1", done: false},
-      {text: "task-2", done: false},
-      {text: "task-3", done: false},
-      {text: "task-4", done: false}
-    ];  
+      { text: 'task-1', done: false },
+      { text: 'task-2', done: false },
+      { text: 'task-3', done: false },
+      { text: 'task-4', done: false },
+    ];
     const localItem = JSON.parse(localStorage.getItem('todoList'));
     if (!localItem || localItem.length === 0) {
       setTodoList(defaultTodoList);
     } else {
       setTodoList(localItem);
     }
+  };
+
+  const updateLocalStorage = (item) => {
+    const localItem = JSON.stringify(item);
+    localStorage.setItem('todoList', [localItem]);
   };
 
   const inputTodoHandler = (event) => {
@@ -32,7 +37,7 @@ const TodoBox = () => {
     newTodoList.unshift({
       text: todo,
       done: false,
-    })
+    });
     setTodoList(newTodoList);
     setTodo('');
     updateLocalStorage(newTodoList);
@@ -49,42 +54,34 @@ const TodoBox = () => {
   const changeTodoListHandler = (event) => {
     const index = event.target.id;
     const newTodoList = [...todoList];
-    const text = newTodoList[index]['text'];
-    const done = newTodoList[index]['done'];
-    newTodoList.splice(index, 1, {text: text, done: !done});
+    const text = newTodoList[index].text;
+    const done = !newTodoList[index].done;
+    newTodoList.splice(index, 1, { text, done });
     setTodoList(newTodoList);
     updateLocalStorage(newTodoList);
   };
 
-  const updateLocalStorage = (item) => {
-    const localItem = JSON.stringify(item);
-    localStorage.setItem('todoList', [localItem]);
-  };
-
   useEffect(() => {
-    initTodoList()
-    console.log()
-  }, [])
+    initTodoList();
+  }, []);
 
   return (
     <>
       <Card className="mx-3 mt-3">
         <Card.Body>
-
           {/* TodoFormComponent */}
-        　<TodoForm
+          <TodoForm
             todo={todo}
-            inputTodoHandler={inputTodoHandler} 
+            inputTodoHandler={inputTodoHandler}
             addTodoListHandler={addTodoListHandler}
           />
 
           {/* TodoListComponent */}
-          <TodoList 
+          <TodoList
             todoList={todoList}
             deleteTodoListHandler={deleteTodoListHandler}
             changeTodoListHandler={changeTodoListHandler}
           />
-          
         </Card.Body>
       </Card>
     </>
