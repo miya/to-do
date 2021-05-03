c518a46b2678156b0b0b3c7fa61bc43a390fdadd
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-04 02:08:23 +0900

    fix lint

--- src/components/TodoCount.jsx
+++ src/components/TodoCount.jsx
@@ -7,14 +7,13 @@ const TodoCount = (props) => {
   const [doneCount, setDoneCount] = useState(0);
   const [notDoneCount, setNotDoneCount] = useState(0);
 
-  const getCount = () => {
-    const doneCount = todoList.filter((todo) => { return todo.done }).length;
-    const notDoneCount = todoList.length - doneCount;
-    setDoneCount(doneCount);
-    setNotDoneCount(notDoneCount);
-  };
-
   useEffect(() => {
+    const getCount = () => {
+      const doneCount = todoList.filter((todo) => { return todo.done }).length;
+      const notDoneCount = todoList.length - doneCount;
+      setDoneCount(doneCount);
+      setNotDoneCount(notDoneCount);
+    };
     getCount();
   }, [todoList]);
 
