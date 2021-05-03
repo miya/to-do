7f9c6d64e6f14fdd654cf45115067abe542c433e
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-04 01:15:34 +0900

    Replaced .eslintrc.json with .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'weseek',
+    'weseek/react',
+  ],
+  plugins: [],
+  parserOptions: {},
+  env: {browser: true},
+  globals: {},
+  rules: {
+    'object-curly-newline': ['off']
+  },
+};
+
