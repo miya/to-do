27c656763761aa168c470e321b92765354eea758
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-04 01:51:38 +0900

    create getCount func

--- src/components/TodoCount.jsx
+++ src/components/TodoCount.jsx
@@ -4,8 +4,15 @@ import PropTypes from 'prop-types';
 const TodoCount = (props) => {
   const { todoList } = props;
 
-  const doneCount = useState(0);
-  const notDoneCount = useState(0);
+  const [doneCount, setDoneCount] = useState(0);
+  const [notDoneCount, setNotDoneCount] = useState(0);
+
+  const getCount = () => {
+    const doneCount = todoList.filter((todo) => { return todo.done }).length;
+    const notDoneCount = todoList - doneCount;
+    setDoneCount(doneCount);
+    setNotDoneCount(notDoneCount);
+  };
 
   return (
     <>
