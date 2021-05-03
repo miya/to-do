4a7b87ca4704f68b9d878133ccd064621deaf400
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-04 01:25:50 +0900

    add state

--- src/components/TodoCount.jsx
+++ src/components/TodoCount.jsx
@@ -1,9 +1,12 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const TodoCount = (props) => {
   const { todoList } = props;
-  console.log(todoList);
+
+  const doneCount = useState(0);
+  const notDoneCount = useState(0);
+
   return (
     <>
       <p>TodoCountComponent</p>
