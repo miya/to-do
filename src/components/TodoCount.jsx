49ff514823ab663163f73425e379e3c343561b46
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-04 00:28:34 +0900

    add props

--- src/components/TodoCount.jsx
+++ src/components/TodoCount.jsx
@@ -2,7 +2,17 @@ import { React } from 'react';
 import PropTypes from 'prop-types';
 
 const TodoCount = (props) => {
+  const { todoList } = props;
+  console.log(todoList);
+  return (
+    <>
+      <p>TodoCountComponent</p>
+    </>
+  );
+};
 
+TodoCount.propTypes = {
+  todoList: PropTypes.array,
 };
 
 export default TodoCount;
