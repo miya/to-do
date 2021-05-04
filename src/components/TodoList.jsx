c317ffe427fc3af3164eb9597d00d6dd762dddd5
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-04 11:46:18 +0900

    add flex-row-reverse class

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -10,7 +10,7 @@ const TodoList = (props) => {
     <>
       {todoList.length !== 0 && (
         <Card className="mt-3">
-          <ListGroup>
+          <ListGroup className="flex-column-reverse">
             {todoList.map((todo, index) => {
               return (
                 <TodoItem
