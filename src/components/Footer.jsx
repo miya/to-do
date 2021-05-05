a3a72cc286e6ddb3cc1c2119d01de14ebd0efbf2
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-05 15:25:33 +0900

    add extra-lint-icon class

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="footer my-2">
       <small>
         <a className="text-secondary" href="https://github.com/miya/to-do">Github</a>
-        <FontAwesomeIcon className="text-secondary mb-1 ml-1" icon={faExternalLinkAlt} />
+        <FontAwesomeIcon className="text-secondary extra-link-icon mb-1 ml-1" icon={faExternalLinkAlt} />
         <text> / </text>
       </small>
       <small className="text-secondary">Copyright (C) 2021 to-do</small>
