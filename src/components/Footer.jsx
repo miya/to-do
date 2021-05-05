cf2df534874fce1fc9aaecaf02197993b27e2681
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-05 15:24:58 +0900

    add extra-link icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="footer my-2">
+      <small>
+        <a className="text-secondary" href="https://github.com/miya/to-do">Github</a>
+        <FontAwesomeIcon className="text-secondary mb-1 ml-1" icon={faExternalLinkAlt} />
+        <text> / </text>
+      </small>
       <small className="text-secondary">Copyright (C) 2021 to-do</small>
     </footer>
   );
