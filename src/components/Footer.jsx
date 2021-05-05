110ac0326a6835020dc3e79c42ac31620da5af6f
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-05 18:22:13 +0900

    fix margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="footer my-2">
       <small>
         <a className="text-secondary" href="https://github.com/miya/to-do">Github</a>
-        <FontAwesomeIcon className="text-secondary extra-link-icon mb-1 ml-1 mr-2" icon={faExternalLinkAlt} />
+        <FontAwesomeIcon className="text-secondary extra-link-icon mb-1 ml-1 mr-3" icon={faExternalLinkAlt} />
       </small>
       <small className="text-secondary">Copyright (C) 2021 to-do</small>
     </footer>
