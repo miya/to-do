e13eca56d64678dff49d7d92e93f94d3ff7650db
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-31 19:55:06 +0900

    make display edit-input

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 import { ListGroup, InputGroup, FormControl } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,6 +7,8 @@ import db from '../utils/db';
 
 const TodoItem = (props) => {
   const { todo, index, todoList, setTodoList } = props;
+
+  const [edit, setEdit] = useState(false);
 
   const deleteTodoListHandler = (id, index) => {
     const newTodoList = [...todoList];
@@ -29,17 +31,19 @@ const TodoItem = (props) => {
       <InputGroup className="align-items-center">
         <input id={index} type="checkbox" className="mr-2" checked={todo.done} onChange={() => updateTodoListHandler(todo.id, index)}></input>
 
-        {/* <FormControl className="mr-2" placeholder="edit todo"/> */}
-
-        {todo.done && (
-        <strike>{todo.text}</strike>
+        {edit && (
+          <FormControl className="mr-2" placeholder="edit todo"/>
         )}
-        {!todo.done && (
+
+        {(todo.done && !edit) && (
+          <strike>{todo.text}</strike>
+        )}
+        {(!todo.done && !edit) && (
           <>{todo.text}</>
         )}
 
         <div className="ml-auto">
-          <button type="button" className="item-update-btn">
+          <button type="button" className="item-update-btn" onClick={() => setEdit(!edit)}>
             <FontAwesomeIcon icon={faEdit} color="#cccccc" />
           </button>
 
