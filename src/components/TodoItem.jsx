cc299293b60460af342f67c2e14eb7d41b375b04
Shun Miyazawa <shun-m@weseek.co.jp>
2021-06-03 12:00:36 +0900

    fix

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -73,7 +73,7 @@ const TodoItem = (props) => {
         )}
 
         <div className="ml-auto">
-          <OverlayTrigger placement="top" transition={false} overlay={<Tooltip id="Tooltip">Edit todo</Tooltip>} >
+          <OverlayTrigger placement="top" transition={false} overlay={<Tooltip id="Tooltip">Edit Todo</Tooltip>} >
             {({ ref, ...triggerHandler }) => (
               <button ref={ref} {...triggerHandler} type="button" className="item-update-btn" onClick={() => onEditButtonPushed()}>
                 <FontAwesomeIcon icon={faEdit} color="#cccccc" />
