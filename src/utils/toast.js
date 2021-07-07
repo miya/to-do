e172d343e670d01101e37a02b19db0ce2ce202d9
Shun Miyazawa <shun-m@weseek.co.jp>
2021-07-07 10:28:17 +0900

    create toastr

---
+++ src/utils/toast.js
@@ -0,0 +1,16 @@
+import toastr from 'toastr';
+
+const toastrOptions = {
+  error: {
+    closeButton: true,
+    progressBar: true,
+    newestOnTop: false,
+    showDuration: '100',
+    hideDuration: '100',
+    timeOut: '0',
+  }
+}
+
+export const toastError = (err, header = 'Error', option = toastrOptions.error) => {
+  toastr.error(err.message, header,option);
+};
