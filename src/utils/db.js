7251b57b14a21b96a62ce04e5728f8473a380342
Shun Miyazawa <shun-m@weseek.co.jp>
2021-07-07 11:10:44 +0900

    class

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,11 +2,29 @@ import Dexie from 'dexie';
 
 const dbName = 'todoList';
 const dbVersion = 1;
+export default class DB {
 
-const db = new Dexie(dbName);
+  constructor() {
+    this.db = new Dexie(dbName);
 
-db.version(dbVersion).stores({
-  todoList: '++id, text, done, created_at',
-});
+    this.db.version(dbVersion).stores({
+      todoList: '++id, text, done, created_at',
+    });
+  }
 
-export default db;
+  async getTodoList() {
+    return await this.db.todoList.toArray()
+  }
+
+  async add(value) {
+    await this.db.todoList.add(value);
+  }
+
+  update(id, key, value) {
+    this.db.todoList.update(id, { [key]: value });
+  }
+
+  delete(id) {
+    this.db.todoList.delete(id);
+  };
+};
